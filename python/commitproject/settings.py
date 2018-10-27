514da096ecb346d90603149f5851258990e7afbb
jorge jardines <gardenunez@gmail.com>
2018-10-27 11:48:17 +0200

    Add Bearer as keyword for TokenAuthentication

--- python/commitproject/settings.py
+++ python/commitproject/settings.py
@@ -51,7 +51,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'pyapi.custom_token_authentication.CustomTokenAuthentication',
     ),
 }
 
