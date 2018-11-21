aee5cd0a284ef3e4229a0559a6fdd24271183f64
jorge j <gardenunez@gmail.com>
2018-11-21 14:55:45 +0100

    Remove commented code

--- python/commitproject/urls.py
+++ python/commitproject/urls.py
@@ -3,7 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # url(r'^api/haters$', views.hater_list),
-    # url(r'^api/haters/(?P<pk>[0-9]+)$', views.hater_detail),
     path('api/', include('pyapi.urls'))
 ]
